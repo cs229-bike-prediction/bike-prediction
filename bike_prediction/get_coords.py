0ed32cad509b3b07a792da8fdc8dc283ac3f74b5
Jeff Wheeler <jeffwheeler@gmail.com>
2014-11-12 15:39:13 -0800

    Add prev_day feature

--- bike_prediction/get_coords.py
+++ bike_prediction/get_coords.py
@@ -2,7 +2,6 @@ import math
 import xml.etree.ElementTree as ET
 
 known_locs = {}
-known_dists = {}
 
 stations = ET.parse('cycle_locations.xml').getroot().findall('station')
 
@@ -14,23 +13,9 @@ def load_all_stations():
         known_locs[int(station.find('id').text)] = r
 
 def get_station_location(station_id):
-    if known_locs.has_key(station_id):
-        return known_locs[station_id]
-    else:
-        for station in stations:
-            if int(station.find('id').text) == station_id:
-                r = (station.find('name').text,
-                        float(station.find('lat').text),
-                        float(station.find('long').text))
-                known_locs[station_id] = r
-                return r
+    return known_locs[station_id]
 
 def get_station_distances(id1, id2):
-    if known_dists.has_key((id1, id2)):
-        return known_dists[(id1, id2)]
-    else:
-        (name1, x1, y1) = get_station_location(id1)
-        (name2, x2, y2) = get_station_location(id2)
-        r = math.sqrt(math.pow(x2-x1, 2) + math.pow(y2-y1, 2))
-        known_dists[(id1, id2)] = r
-        return r
+    (name1, x1, y1) = get_station_location(id1)
+    (name2, x2, y2) = get_station_location(id2)
+    return math.sqrt(math.pow(x2-x1, 2) + math.pow(y2-y1, 2))
--- bike_prediction/read_data.py
+++ bike_prediction/read_data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from pandas.tseries.offsets import Day
 
 from bike_prediction import get_coords as gc
 
@@ -16,9 +17,10 @@ def dist(r):
         return np.nan
 
 def f(r):
+    # Disable some of these to improve performance
     return pd.Series({'count': r['Rental Id'].count(),
                       'mean_duration': r['Duration'].mean(),
-                      'mean_distance': r.loc[:,('StartStation Id','EndStation Id')].apply(dist, axis=1).mean()
+                      # 'mean_distance': r.loc[:,('StartStation Id','EndStation Id')].apply(dist, axis=1).mean()
                       })
 
 def get_rides():
@@ -27,15 +29,31 @@ def get_rides():
                 skipinitialspace=True,
                 index_col='Start Date',
                 parse_dates=True,
-                dayfirst=True)
+                dayfirst=True,
+                usecols=['Rental Id', 'Start Date', 'Duration', 'StartStation Name', 'StartStation Id', 'EndStation Id'],
+            )
     rides = rides[rides['Duration'] > 0]
     return rides
 
 def read_data(rides):
-    g = rides.groupby([pd.Grouper(freq='30T', level='Start Date'), 'StartStation Name']).apply(f).reset_index().set_index('Start Date')
+    r = rides.groupby([pd.Grouper(freq='30T', level='Start Date'), 'StartStation Name']).apply(f).reset_index()
+    return r.pivot(index='Start Date', columns='StartStation Name').fillna(value=0)
 
+def add_weather_features(X):
     weather = pd.read_csv(open('./weather.csv', 'rb'), parse_dates=True, index_col='GMT')
     temps = weather['Mean TemperatureF']
 
-    rw = pd.concat([g, temps['2013']], axis=1, join_axes=[g.index]).fillna(method='pad')    
-    return rw
+    return pd.concat([X, temps['2013']], axis=1, join_axes=[X.index]).fillna(method='pad')
+
+def add_historic_features(usage):
+    shifted = usage['count'].shift(freq=Day(1))
+    prev_day = pd.DataFrame(shifted.stack(), columns=['prev_day']).unstack()
+    with_history = pd.concat([usage, prev_day], axis=1)
+    return with_history
+
+def filter_unknown_history(X):
+    f = X[np.isfinite(X['prev_day'])]
+    return f[np.isfinite(f['count'])]
+
+def save_sheet(df):
+    df.to_excel('test.xls')
